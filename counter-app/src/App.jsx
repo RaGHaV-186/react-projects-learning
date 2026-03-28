82ef75f50c27836554f6b85c5395f63a06fff6bf
Raghav Bandreddi <raghavbandreddy@gmail.com>
2026-03-29 02:02:26 +0530

    updated counter-app

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -7,14 +7,20 @@ function App(){
     setCount((prev)=>prev+1)
   }
   function decrease(){
+    if (count>0){
     setCount((prev)=>prev-1)
+    }
+  }
+  function reset(){
+    setCount(0)
   }
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{count}</h2>
       <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
+      <button onClick={decrease} disabled={count===0}>Decrease</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 }
