30365f507131abe09a93364a5c1c26c4e5540f15
Raghav Bandreddi <raghavbandreddy@gmail.com>
2026-03-31 16:42:16 +0530

    Added window-resize

---
+++ window-resize/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Height: {height}</h1>
+      <h1>Width: {width}</h1>
+    </div>
+  );
+}
+
+export default App;
